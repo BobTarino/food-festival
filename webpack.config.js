63f71910fd21f88f00e894614d6516b35aaa4bb4
Bobby T <bob.tarino@gmail.com>
2021-07-27 12:42:13 -0500

    added image optimizer loader to reduce image file size

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,43 @@ const path = require('path');
 const webpack = require("webpack");
 
 module.exports = {
-  entry: './assets/js/script.js',
+  // entry points so webpack will know where to start the bundle of dependecies 
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    // add file-loader to webpack configuration - will identify files to pre-process using test property to find regex matching a .jpg file
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            // return file with proper path and file name
+            options: {
+              esModule: false,
+              name (file) {
+                return "[path][name].[ext]"
+              },
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }  
+          },
+          // reduce image size with image optimizer loader
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins:[
     new webpack.ProvidePlugin({
