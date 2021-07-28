46932f6dace08efad7634c6d5a94dbb9c326f789
Bobby T <bob.tarino@gmail.com>
2021-07-28 16:31:43 -0500

    added offline functionality

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
-const APP_PREFIX = 'FoodFest-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+// first code that runs in the application for offline functionality
 const FILES_TO_CACHE = [
     "./index.html",
     "./events.html",
@@ -14,6 +12,11 @@ const FILES_TO_CACHE = [
     "./dist/tickets.bundle.js",
     "./dist/schedule.bundle.js"
 ];
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+// string combo of app prefix and version variable // for software iterations when changing version number
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 
 
 self.addEventListener('install', function (e) {
@@ -36,6 +39,7 @@ self.addEventListener('activate', function(e) {
         // resolves once all old versions of the cache have been deleted
         return Promise.all(
           keyList.map(function(key, i) {
+            //   will only return value of -1 if item is not found in keeplist
             if (cacheKeeplist.indexOf(key) === -1) {
               console.log('deleting cache : ' + keyList[i]);
               return caches.delete(keyList[i]);
@@ -45,3 +49,23 @@ self.addEventListener('activate', function(e) {
       })
     );
 });
+
+// application knows to retrieve info from cache
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    // intercept fetch request
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
